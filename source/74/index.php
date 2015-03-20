94fd69171e6884a21c156f07ef0b864a7d2ca054
bin liu <liubin@pe.vc>
2015-03-20 04:35:28 +0000

    update

--- source/74/index.php
+++ source/74/index.php
@@ -2,12 +2,12 @@
   session_start();
 ?>
 <html>
-<head><title>7. 携帯電話向けWebアプリケーションのセキュリティ</title></head>
+<head><title>7. 面向手机的Web应用程序安全</title></head>
 <body>
 <b>注：中文版本章已经删除，仅供参考</b>
-7. 携帯電話向けWebアプリケーションのセキュリティ<br>
-7.4 URL埋め込みのSession IDによる問題<br><br>
-<a href="74-001.php">74-001:クッションページのデモ</a><br><br>
+7. 面向手机的Web应用程序安全<br>
+7.4 嵌入到URL中的Session ID引起的安全问题<br><br>
+<a href="74-001.php">74-001:缓冲页面DEMO</a><br><br>
 <a href="phpinfo.php">phpinfo</a><br>
 <a href="/">返回首页</a>
 </body>
